0f1fbe479ef0d963d012b8615c7ed515a07217ed
devoliveiras <guilhermeao@outlook.com>
2022-06-23 14:41:14 -0300

    Protegendo método template para não ser chamado fora da classe.

--- app/views/mensagemView.ts
+++ app/views/mensagemView.ts
@@ -2,7 +2,7 @@ import { View } from "./View.js";
 
 export class MensagemView extends View<String>{
 
-    template(model: string): string{
+    protected template(model: string): string{
         return `
             <p class="alert alert-success">${model}</p>
         `
